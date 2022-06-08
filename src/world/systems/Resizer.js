32c72742a37be47c33b17bc6a01a0f5bc39fb96f
doremi31618 <doremi31618@gmail.com>
2022-06-08 17:06:34 +0800

    add resizing window and anti-aliasing

--- src/world/systems/Resizer.js
+++ src/world/systems/Resizer.js
@@ -3,13 +3,22 @@
 //2. resetting camera projection matrix
 //3. setting renderer & camvas size
 //4. (specific mobile) set the pixel ratio to prevent bluring output canvas
+const setSize = (container, camera, renderer) => {
+    camera.aspect = container.clientWidth/ container.clientHeight;
+    camera.updateProjectionMatrix();
+    
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+}
 class Resizer{
     constructor(container, camera, renderer){
-        camera.aspect = container.clientWidth/ container.clientHeight;
-        camera.updateProjectionMatrix();
-        
-        renderer.setSize(container.clientWidth, container.clientHeight);
-        renderer.setPixelRatio(window.devicePixelRatio);
+        setSize(container, camera, renderer);
+        window.addEventListener('resize', ()=>{
+            this.onResize();
+        })
+    }
+    onResize(){
+        //re-render scene
     }
 }
 
--- src/world/systems/renderer.js
+++ src/world/systems/renderer.js
@@ -2,7 +2,8 @@ import {WebGLRenderer} from 'https://cdn.skypack.dev/three@0.141.0';
 
 //create renderer
 function createRenderer(){
-    const renderer = new WebGLRenderer();
+    const renderer = new WebGLRenderer({antialias: true});
+    renderer.physicallyCorrectLights = true;
     return renderer;
 }
 
