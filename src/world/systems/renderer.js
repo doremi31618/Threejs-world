c33742b721beb4f3694470e6395432e9e0d16a06
doremi31618 <doremi31618@gmail.com>
2022-08-08 15:10:40 +0800

    add premultiplied alpha

--- src/world/systems/renderer.js
+++ src/world/systems/renderer.js
@@ -2,8 +2,11 @@ import {WebGLRenderer} from 'three';
 
 //create renderer
 function createRenderer(){
-    const renderer = new WebGLRenderer({antialias: true, alpha: true});
-    // renderer.setClearColor(0xffffff, 0);
+    const renderer = new WebGLRenderer({
+        antialias: true,
+        alpha: true,
+        premultipliedAlpha: false
+        });
     renderer.physicallyCorrectLights = true;
    
     return renderer;
